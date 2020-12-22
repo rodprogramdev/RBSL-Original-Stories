b6b9fb4043ccf0ca1ecac80ba63b496dca53c1a3
Rodrigo Lopez <rodprogramdev@gmail.com>
2020-12-22 20:26:39 +0800

    Add all files

---
+++ welcome.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WELCOME ROD</title>
+    <style>
+          * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body{
+            font-family: 'Chewy', cursive;
+            font-size: 20px;
+        }
+        
+        .header{
+            background-color: rgb(16,84,211);
+            height: 20vh;
+            color:white;
+            text-align:center;
+            
+        }
+      
+    </style>
+    
+        
+</head>
+<body>
+    <header>
+        <div class="header">
+            <div class="header__heading">
+                <h1 class="header__heading--1">WELCOME!!</h1>
+            </div>
+            <div class="log-out">
+                <a href="index.html">Log out</a>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
